8f28c5a7ab98037a540800438afb39c8985c88f5
Folahanmi <emmanuelkolawole4@gmail.com>
2019-09-19 13:01:02 +0100

    latest progress

--- src/js/index.js
+++ src/js/index.js
@@ -21,9 +21,7 @@ $(document).ready(function() {
             alert('please ensure password and confirm password fields are the same');
         }
         else {
-            $.post("http://localhost:3000/users", data, function() {
-                window.location.replace("profile.html");
-            });
+            $.post("http://localhost:3000/users", data, location.replace('profile.html'));
         }
     })
 
@@ -47,15 +45,44 @@ $(document).ready(function() {
         })
     });
 
+    // Read Listing on SignIn
     $.get("http://localhost:3000/properties", function(data) {
-        for (let obj of data) {
+        for (let object of data) {
             $('#featured-properties1').append(`
-            <div style="border-bottom: 1px solid; padding: 10px; height: 100px; margin-bottom: 10px;" class="col-12">
-                <h3>Property Type: ${obj.propertytype}</h3>
-                <h5>number of rooms: ${obj.rooms}</h5>
+            <div style="border-bottom: 1px solid; padding: 0; height: 100px; margin-bottom: 10px;" class="col-12">
+                <h3>Property Type: ${object.propertytype}</h3>
+                <h5>number of rooms: ${object.rooms}</h5>
+                <h5>number of rooms: ${object.location}</h5>
             </div>
             `);
         }            
     })
+
+    // Populate Select Property list from the backend
+    $.get("http://localhost:3000/defaultproperties", function(data) {
+        for (let object of data) {
+            let { propertytypes } = object;
+            for (houses of propertytypes) {
+                $('#select-property').append(`<option value='${houses}'>${houses}</option`);
+
+            }
+        }
+    })
+
+    // Populate DB.Json with property details from the list property form fields
+    $('#listprop').click((e) => {
+        e.preventDefault();
+        let propertyTypeValue = $('#select-property').children('option:selected').val();
+        let numberOfRoomsValue = $('#select-number-of-rooms').children('option:selected').val();
+        let locationValue = $('#location').val();
+
+        let newData = {
+            propertytype: propertyTypeValue,
+            rooms: numberOfRoomsValue,
+            location: locationValue
+        }
+        $.post("http://localhost:3000/properties", newData, alert('property added'));
+    })
+    
     
 })
